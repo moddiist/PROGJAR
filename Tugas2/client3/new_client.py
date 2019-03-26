55020d5346909fc897332ae3bab891005ceca02e
chen1704 <moddiist@gmail.com>
2019-03-26 15:12:04 +0700

    upload

---
+++ Tugas2/client3/new_client.py
@@ -0,0 +1,31 @@
+import socket
+
+
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto("READY", (SERVER_IP, SERVER_PORT))
+counter=1
+
+def getImage():
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if(data[0:5]=="START"):
+            print data[6:]
+            fp = open(data[6:],'wb+')
+            ditulis=0
+        elif(data=="FINISH"):
+            fp.close()
+        elif(data=="ENDING"):
+            break
+        else:
+            print "blok ", len(data), data[0:10]
+            fp.write(data)
+
+while counter==1:
+    data, addr = sock.recvfrom(1024)
+    if(data=="SENDING"):
+        getImage()
+        counter = 0
+
---
+++ Tugas2/new_server.py
@@ -0,0 +1,37 @@
+from threading import Thread
+import socket
+import os
+
+TARGET_IP = "127.0.0.1"
+TARGET_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((TARGET_IP, TARGET_PORT))
+namafile=["bart.png", "maggie.jpg", "lisa.jpg", "homer.jpg"]
+
+def sendImage(CLIENT_IP, CLIENT_PORT):
+   addr = CLIENT_IP, CLIENT_PORT
+   sock.sendto("SENDING" , (addr))
+   for nama in namafile:
+      sock.sendto("START {}" . format(nama) , (addr))
+      ukuran = os.stat(nama).st_size
+      fp = open(nama,'rb')
+      k = fp.read()
+      terkirim=0
+      for x in k:
+         sock.sendto(x, (addr))
+         terkirim = terkirim + 1
+         print "\r terkirim {} of {} " . format(terkirim,ukuran)
+      sock.sendto("FINISH", (addr))
+      fp.close()
+   sock.sendto("ENDING", (addr))
+
+
+while True:
+   print "Waiting..."
+   data, addr = sock.recvfrom(1024)
+   if(data=="READY"):
+      thread = Thread(target=sendImage, args=(addr))
+      thread.start()
+      
+
